5e0279ff947b1a986dc8e4125c99c4e8a4c514a3
Oindrila Das <>
2024-10-25 16:25:02 +0530

    Drag and Drop

--- Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
+++ Assets/Scripts/UI/MonkeySelectionPanel/MonkeyImageHandler.cs
@@ -6,11 +6,16 @@ using UnityEngine.UI;
 
 namespace ServiceLocator.UI
 {
-    public class MonkeyImageHandler : MonoBehaviour
+    public class MonkeyImageHandler : MonoBehaviour,IDragHandler, IEndDragHandler, IPointerDownHandler
     {
         private Image monkeyImage;
         private MonkeyCellController owner;
         private Sprite spriteToSet;
+        private RectTransform rectTransform;
+        private Vector2 originalPos;
+
+        private Color defaultCol = Color.white;
+       
 
         public void ConfigureImageHandler(Sprite spriteToSet, MonkeyCellController owner)
         {
@@ -22,6 +27,38 @@ namespace ServiceLocator.UI
         {
             monkeyImage = GetComponent<Image>();
             monkeyImage.sprite = spriteToSet;
+            rectTransform = GetComponent<RectTransform>();
+            originalPos = rectTransform.position;
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            rectTransform.position += (Vector3)eventData.delta;
+            owner.MonkeyDraggedAt(rectTransform.position);
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            ResetMonkey();
+            owner.MonkeyDroppedAt(eventData.position);
+        }
+
+        private void ResetMonkey()
+        {
+            ChangeMonkeyColour(defaultCol);
+            rectTransform.position = originalPos;
+            GetComponent<LayoutElement>().enabled = false;
+            GetComponent<LayoutElement>().enabled = true;
+        }
+
+        private void ChangeMonkeyColour(Color color)
+        {
+            monkeyImage.color = color;
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            ChangeMonkeyColour(Color.blue);
         }
     }
 }
